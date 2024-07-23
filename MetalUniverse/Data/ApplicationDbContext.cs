0ee822305b76ed0f47582c7f131291868ddcc37b
Qwerty8838 <mnikolov883@gmail.com>
2024-07-23 12:24:33 +0300

    Adding sql solution file I am using for the database in MSSQL studio

--- MetalUniverse/Data/ApplicationDbContext.cs
+++ MetalUniverse/Data/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using MetalUniverse.Models;
 
 namespace MetalUniverse.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Product> Products { get; set; }
+        // Add other DbSets for additional entities.
     }
 }
+
